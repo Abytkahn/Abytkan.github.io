6981939ee74981f12b322df5ee31aafe9ce45cfa
Your Name <you@example.com>
2021-03-01 22:48:11 +0600

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bonus</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+    <link rel="stylesheet"  href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+		<section class="banner">
+		<div class="textBx">
+			<h2>Hello, I'm<br><span>Abytkhan.</span></h2>
+			<h3>I'm a database master in Kaskelen <br>I build operation system and mobile aplications.</h3>
+			<a href="190103257@stu.sdu.edu.kz" class="btn">Get In Touch</a>
+		</div>
+	</section>
+		<section class="about" id="about">
+			<div class="heading">
+				<h2>About Me</h2>
+			</div>
+			<div class="content">
+				<div class="contentBx w50">
+					<h3>I'm database master</h3>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. <br><br>
+					Shortly I learning in SDU university. <br>
+					Here are a few technologies I've been working with recently:
+					<br>
+					<br>
+					</p>
+					<p>C++</p>
+					<p>HTML & CSS</p>
+					<p>Javascript</p>
+					<p>Java</p>
+					<p>Python</p>
+					<a href="#" class="btn">Download CV</a>
+				</div>
+				<div class="w50">
+					<img src="C:\Users\Hp\Desktop\Мои фотки\me.JPG" class="img">
+				
+				</div>
+			</div>
+			<section class="services" id="services">
+				<div class="max-width">
+					<h2 class="title">What Can I Do</h2>
+						<div class="serv-content">
+							<div class="card">
+								<div class="box">
+									<i class="fa fa-mobile"></i>
+									<div class="text">App Developing</div>
+									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam nisi sequi voluptates illum quibusdam quo.</p>
+								</div>
+							</div>
+							<div class="card">
+								<div class="box">
+									<i class="fa fa-code"></i>
+									<div class="text">Apps Design</div>
+									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam nisi sequi voluptates illum quibusdam quo.</p>
+								</div>
+							</div>
+							<div class="card">
+								<div class="box">
+									<i class="fa fa-desktop"></i>
+									<div class="text">UI&UX Design</div>
+									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam nisi sequi voluptates illum quibusdam quo.</p>
+								</div>
+							</div>
+
+						</div>
+				</div>
+
+			</section>
+
+				<section class="contact" id="contact">
+        			<div class="max-width">
+            		<h2 class="title">Contact me</h2>
+						<div class="contact-content">
+                			<div class="column left">
+                    		
+								<p>
+                        				<div class="row">
+                            			<i class="fa fa-user"></i>
+                            				<div class="info">
+                                				<div class="head">Name</div>
+													<div class="sub-title">Srazhiddin Abytkhan</div>
+											</div>
+										</div>
+														<div class="row">
+                            							<i class="fa fa-map-marker"></i>
+                            								<div class="info">
+                                								<div class="head">Address</div>
+																<div class="sub-title">Kaskelen, Kazakhstan</div>
+															</div>
+														</div>
+															<div class="row">
+                            									<i class="fa fa-envelope"></i>
+                            										<div class="info">
+                                										<div class="head">Email</div>
+																			<div class="sub-title">Srazhiddinabytkhan@gmail.com</div>
+																			
+																	</div>
+
+															</div>
+									</div>
+							</div>
+                				<div class="column right" >
+                    				<div class="text" style="margin-bottom:20px;">Message me</div>
+									<form>
+                        				<div class="fields">
+                            				<div class="field name">
+                               				<input  type="text" placeholder="Name" required>
+                            				</div>
+                            					<div class="field email">
+                                				<input  type="email" placeholder="Email" required>
+                           						</div>
+                        				</div>
+
+                        					<div class="field textarea">
+                           
+                            				<textarea cols="20" rows="10"  placeholder="Message.." required></textarea>
+                        					</div>
+                        						<div class="button">
+                            					<button type="submit">Send message</button>
+                        						</div>
+									</form> 	
+                				</div>
+                		</div>
+
+					</div>
+				</section>
+
+					<footer>
+						<div class="copyright">
+							Designed & Built by Abytkhan
+						</div>
+					</footer>
+
+		<script src="script.js"></script>
+</body>
+</html>
