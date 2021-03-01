96590c85e58bd022266f90801bfe8df360120e9b
Your Name <you@example.com>
2021-03-01 23:28:37 +0600

    commit finish

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('post/create', function () {
+    DB::table('post ')->insert([
+        'title' => 'TITLE',
+        'body' => 'BODY', 
+    ]);
+});
+Route::get('post', function () {
+    $post = Post::find(1);
+    return $post;
 });
